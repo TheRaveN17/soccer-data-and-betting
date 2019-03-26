34b8789630e9bc88822d6c00f66a88338ea64d31
Raven <AsnakeA007@gmail.com>
2019-03-26 09:18:58 +0200

    db vs 0.32
    --added extra models Defender, Midfielder, Forward --> one to one relationship with Player

--- database/models.py
+++ database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Unicode, ForeignKey
+from sqlalchemy import Column, Integer, String, Unicode, ForeignKey, Float
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -22,3 +22,68 @@ class Team(Base):
     name = Column('name', Unicode(50))
     url = Column('url', Unicode(100))
     players = relationship(Player, backref=backref('team'))
+
+class Defender(Base):
+    __tablename__ = 'defender'
+    xp = Column('xp', Integer())
+    apps = Column('apps', Integer())
+    tackling = Column('tackling', Float(precision=2))
+    interceptions = Column('interceptions', Integer())
+    clearances = Column('clearances', Integer())
+    crossesBlocked = Column('crossesBlocked', Integer())
+    passesBlocked = Column('passesBlocked', Integer())
+    aerial = Column('aerial', Float(precision=2))
+    goals = Column('goals', Integer())
+    setPieceGoals = Column('setPieceGoals', Integer())
+    shortPassAccuracy = Column('shortPassAccuracy', Float(precision=2))
+    longPassAccuracy = Column('longPassAccuracy', Float(precision=2))
+    totalCrosses = Column('totalCrosses', Integer())
+    crossAccuracy = Column('crossAccuracy', Float(precision=2))
+    keyPasses = ('keyPasses', Integer())
+    assists = Column('assists', Integer())
+    player = relationship(Player, backref=backref('defender', uselist=False))
+
+class Midfielder(Base):
+    __tablename__ = 'midfielder'
+    xp = Column('xp', Integer())
+    apps = Column('apps', Integer())
+    shotsOnTarget = Column('shotsOnTarget', Integer())
+    shotsOffTarget = Column('shotsOffTarget', Integer())
+    shotsBlocked = Column('shotsBlocked', Integer())
+    shotsPost = Column('shotsPost', Integer())
+    dribbling = Column('dribbling', Float(precision=2))
+    tackling = Column('tackling', Float(precision=2))
+    interceptions = Column('interceptions', Integer())
+    clearances = Column('clearances', Integer())
+    crossesBlocked = Column('crossesBlocked', Integer())
+    passesBlocked = Column('passesBlocked', Integer())
+    aerial = Column('aerial', Float(precision=2))
+    goals = Column('goals', Integer())
+    setPieceGoals = Column('setPieceGoals', Integer())
+    shortPassAccuracy = Column('shortPassAccuracy', Float(precision=2))
+    longPassAccuracy = Column('longPassAccuracy', Float(precision=2))
+    totalCrosses = Column('totalCrosses', Integer())
+    crossAccuracy = Column('crossAccuracy', Float(precision=2))
+    keyPasses = ('keyPasses', Integer())
+    assists = Column('assists', Integer())
+    player = relationship(Player, backref=backref('midfielder', uselist=False))
+
+class Forward(Base):
+    __tablename__ = 'forward'
+    xp = Column('xp', Integer())
+    apps = Column('apps', Integer())
+    shotsOnTarget = Column('shotsOnTarget', Integer())
+    shotsOffTarget = Column('shotsOffTarget', Integer())
+    shotsBlocked = Column('shotsBlocked', Integer())
+    shotsPost = Column('shotsPost', Integer())
+    dribbling = Column('dribbling', Float(precision=2))
+    aerial = Column('aerial', Float(precision=2))
+    goals = Column('goals', Integer())
+    setPieceGoals = Column('setPieceGoals', Integer())
+    shortPassAccuracy = Column('shortPassAccuracy', Float(precision=2))
+    longPassAccuracy = Column('longPassAccuracy', Float(precision=2))
+    totalCrosses = Column('totalCrosses', Integer())
+    crossAccuracy = Column('crossAccuracy', Float(precision=2))
+    keyPasses = ('keyPasses', Integer())
+    assists = Column('assists', Integer())
+    player = relationship(Player, backref=backref('forward', uselist=False))
